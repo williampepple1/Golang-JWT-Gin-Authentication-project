fd0fd3b4492a4f749e22178e662e928c63cdd5d0
William Pepple <williampepple1@gmail.com>
2023-10-31 09:54:53 +0100

    update

--- main.go
+++ main.go
@@ -1,14 +1,16 @@
 package main
 
-import(
-	routes "github.com/akhil/golang-jwt-project/routes"
-	"os"
+import (
 	"log"
+	"os"
+
+	"github.com/akhil/golang-jwt-project/middleware"
+	routes "github.com/akhil/golang-jwt-project/routes"
 	"github.com/gin-gonic/gin"
 	"github.com/joho/godotenv"
 )
 
-func main(){
+func main() {
 	err := godotenv.Load(".env")
 
 	if err != nil {
@@ -16,23 +18,23 @@ func main(){
 	}
 	port := os.Getenv("PORT")
 
-	if port==""{
-		port="8000"
+	if port == "" {
+		port = "8000"
 	}
 
 	router := gin.New()
 	router.Use(gin.Logger())
-
+	router.Use(middleware.Authenticate())
 	routes.AuthRoutes(router)
 	routes.UserRoutes(router)
 
-	router.GET("/api-1", func(c *gin.Context){
-		c.JSON(200, gin.H{"success":"Access granted for api-1"})
+	router.GET("/api-1", func(c *gin.Context) {
+		c.JSON(200, gin.H{"success": "Access granted for api-1"})
 	})
 
-	router.GET("/api-2", func(c *gin.Context){
-		c.JSON(200, gin.H{"success":"Access granted for api-2"})
+	router.GET("/api-2", func(c *gin.Context) {
+		c.JSON(200, gin.H{"success": "Access granted for api-2"})
 	})
 
 	router.Run(":" + port)
-}	
+}
--- routes/userRouter.go
+++ routes/userRouter.go
@@ -1,12 +1,12 @@
 package routes
 
-import(
+import (
 	controller "github.com/akhil/golang-jwt-project/controllers"
 	"github.com/akhil/golang-jwt-project/middleware"
 	"github.com/gin-gonic/gin"
 )
 
-func UserRoutes(incomingRoutes *gin.Engine){
+func UserRoutes(incomingRoutes *gin.Engine) {
 	incomingRoutes.Use(middleware.Authenticate())
 	incomingRoutes.GET("/users", controller.GetUsers())
 	incomingRoutes.GET("/users/:user_id", controller.GetUser())
